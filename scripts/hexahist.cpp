48f7c3d8a995dc20028966fd149996f8ad9a636b
Matteo Brini <brinimatteo@gmail.com>
2022-04-28 00:37:00 +0200

    Now the script works fine

--- scripts/hexahist.cpp
+++ scripts/hexahist.cpp
@@ -1,3 +1,8 @@
+//
+// Simple plot of log file, run with the following command:
+//      root 'hexahist.cpp(fname)'
+// For example: root 'hexahist.cpp("plot0427.log")'
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -5,21 +10,18 @@
 #include "TH1F.h"
 #include "TCanvas.h"
 
-void hexahist(){
-    std::ifstream fin("../scripts/plot0427.log");
+void hexahist(std::string fname){
+    std::ifstream fin("../data/" + fname);
     std::vector<int> data;
     std::string element;
 
     TCanvas * c1 = new TCanvas("c1", "c1", 1);
-    c1->cd();
-    TH1F * h1 = new TH1F("h1", "h1", 500, 0, 8000);
-    
+    TH1F * h1 = new TH1F("h1", "h1", 50, 0, 8000);
+
     while (fin >> element){
         unsigned int value = std::stoul(element, nullptr, 16);
         data.push_back(value);
         h1->Fill(value);
-        std::cout << "ciao" << std::endl;
     }
-    h1->Fill(200);
-    h1->Draw("E");
+    h1->Draw();
 }
