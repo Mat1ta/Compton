538f9621e26e72552dff9c1fdbc13ec8891dccd6
Matteo Brini <brinimatteo@gmail.com>
2022-05-28 15:46:48 +0200

    increased number of calls for minimization algorithm

--- scripts/fit_2gaus.cpp
+++ scripts/fit_2gaus.cpp
@@ -10,6 +10,8 @@
 #include "TStyle.h"
 #include "TLegend.h"
 #include "TPaveText.h"
+#include "TMatrixD.h"
+#include "TROOT.h"
 
 TPaveText * drawInfos(){
     TPaveText * infos = new TPaveText(.35, .69, .5, .84, "NDC nb");
@@ -17,9 +19,9 @@ TPaveText * drawInfos(){
     infos->SetTextSizePixels(22);
     infos->SetFillColorAlpha(0,0.0);
     infos->SetTextAlign(11);
-    infos->AddText("12th of may");
-    infos->AddText("#theta = 22#circ");
-    infos->AddText("Lenght =  26677 s");
+    infos->AddText("26th of may");
+    infos->AddText("#theta = 18#circ");
+    infos->AddText("Lenght =  30959 s");
     infos->Draw();
     return infos;
 }
@@ -67,7 +69,6 @@ Double_t gaus(Double_t * x, Double_t * p){
 Double_t ufunc(Double_t * x, Double_t * p){
     Double_t g1[] = {p[0], p[1], p[2]};
     Double_t g2[] = {p[3], p[4], p[5]};
-    //return p[0] + p[1]*xx + p[2]*TMath::Gaus(xx, p[3], p[4]) + p[5]*TMath::Gaus(xx, p[6], p[7]);
     return SDAfunc(x, &p[6]) + gaus(x, g1) + gaus(x, g2);
 }
 
@@ -75,14 +76,15 @@ void fit_2gaus(std::string fname){
     gStyle->SetOptStat("e"); gStyle->SetOptFit(1111);
     gStyle->SetStatBorderSize(0.); gStyle->SetTextSize(19);
     gStyle->SetStatX(.92); gStyle->SetStatY(.89);
-
+    ROOT::Math::MinimizerOptions::SetDefaultMaxFunctionCalls(10000);
+    
     std::ifstream fin("../dati/" + fname);
     std::string element;
-    Double_t par[8];
-    Double_t min = 3750, max = 6800;
+    Double_t par[13];
+    Double_t min = 3750, max = 6700;
 
     TCanvas * c1 = new TCanvas("c1", "c1", 1);
-    TH1F * h1 = new TH1F("h1", "h1", 120, 3000, 9000);
+    TH1F * h1 = new TH1F("h1", "h1", 120, 3200, 9500);
 
     while (fin >> element){
         unsigned int value = std::stoul(element, nullptr, 16);
@@ -93,31 +95,30 @@ void fit_2gaus(std::string fname){
     TF1 * gaus2 = new TF1("gaus2", gaus, min, max, 3); gaus2->SetLineColor(3);
     TF1 * fSDA = new TF1("SDA", SDAfunc, min, max, 7); fSDA->SetLineColor(6);
 
-    // func->SetParNames("A_{0}", "#mu_{0}", "#sigma_{0}", "A_{1}", "#mu_{1}", "#sigma_{1}", "a", "b", "c", "E_{c}" ,"#sigma_{c}", "m", "q");
-    Double_t par0[] = {100, 4650, 100, 100, 5200, 100, 2e-5, 2e-4, 1e-3, 3400, 100, -0.01, 1};
+    Double_t par0[] = {300, 5300, 100, 200, 5800, 100, 2e-5, -2e-4, 1e-3, 4000, 500, -0.01, 1};
     func->SetParName(0, "A_{0}"); func->SetParName(1, "#mu_{0}"); func->SetParName(2, "#sigma_{0}");
     func->SetParName(3, "A_{1}"); func->SetParName(4, "#mu_{1}"); func->SetParName(5, "#sigma_{1}");
     func->SetParName(6, "a"); func->SetParName(7, "b"); func->SetParName(8, "c");
     func->SetParName(9, "E_{C}"); func->SetParName(10, "#sigma_{C}");
     func->SetParName(11, "m"); func->SetParName(12, "q");
     func->SetParameters(par0);
-    func->SetParLimits(0, 100, 300);   // A_0
-    func->SetParLimits(1, 4700, 4800); // mu_0
+    func->SetParLimits(0, 128, 400);   // A_0
+    func->SetParLimits(1, 5200, 5400); // mu_0
     func->SetParLimits(2, 0, 300);     // sigma_0
-    func->SetParLimits(3, 100, 300);   // A_1
-    func->SetParLimits(4, 4800, 5200); // mu_1
+    func->SetParLimits(3, 0, 260);   // A_1
+    func->SetParLimits(4, 5700, 5950); // mu_1
     func->SetParLimits(5, 0, 300);     // sigma_1
     func->SetParLimits(6, 0, 1e-4);       // a
-    // func->SetParLimits(7, 0, 1);       // b
+    // func->SetParLimits(7, -1, 0);       // b
     // func->SetParLimits(8, 0, 1);       // c
-    func->SetParLimits(9, 3300, 4000); // E_c
+    func->SetParLimits(9, 3900, 4300); // E_c
     func->SetParLimits(10, 0, 600);// sigma_c
     func->SetParLimits(11, -1, 0);     // m
     func->SetParLimits(12, -10, 10);   // q
     //func->FixParameter(11, -1.78e-5);
     //func->FixParameter(12, 4);
 
-    TFitResultPtr r = h1->Fit("func", "","LIRMS", min, max);
+    TFitResultPtr r = h1->Fit(func, "S", " LIRMS",min, max); r->Print();
     func->GetParameters(par);
     Double_t g1[] = {par[0], par[1], par[2]};
     Double_t g2[] = {par[3], par[4], par[5]};
